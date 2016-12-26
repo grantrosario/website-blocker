1dd497293299661ed993f55ea3ddb834325404c2
Grant Rosario <grantster1124@aol.com>
2016-12-25 22:44:03 -0500

    Linked program to working host file on computer

--- website_blocker.py
+++ website_blocker.py
@@ -5,12 +5,12 @@ from datetime import datetime as dt
 hosts_temp = "hosts"
 hosts_path = "/etc/hosts"
 redirect="127.0.0.1"
-website_list=["www.facebook.com","facebook.com","ign.com", "comingsoon.net"]
+website_list=["www.facebook.com","facebook.com","www.ign.com", "www.comingsoon.net"]
 
 while True:
     if dt(dt.now().year,dt.now().month,dt.now().day,8) < dt.now() < dt(dt.now().year,dt.now().month,dt.now().day,16):
         print("Working hours...")
-        with open(hosts_temp,'r+') as file:
+        with open(hosts_path,'r+') as file:
             content = file.read()
             for website in website_list:
                 if website in content:
@@ -18,7 +18,7 @@ while True:
                 else:
                     file.write(redirect + " " + website + "\n")
     else:
-        with open(hosts_temp,'r+') as file:
+        with open(hosts_path,'r+') as file:
             content = file.readlines()
             file.seek(0)
             for line in content:
